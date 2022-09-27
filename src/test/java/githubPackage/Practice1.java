452be0970a72aed8c89db30d34e273d44acf55fe
M H KARIM <M H KARIM@DESKTOP-6T4V6P1>
2022-09-27 15:11:54 +0530

    Project Updated

---
+++ src/test/java/githubPackage/Practice1.java
@@ -0,0 +1,16 @@
+package githubPackage;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	@Test
+	public void loginpage() 
+	{
+		System.out.println("----login to the applucation------");
+	}
+	@Test
+	public void homepage() {
+		System.out.println("-----Welcome to homepage-----------");
+	}
+
+}
